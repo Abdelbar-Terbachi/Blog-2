07c488c1af23d9329a3d77e04df812ed430ae60b
Abdelbar Terbachi <abdelbarterbachi@gmail.com>
2021-08-26 11:08:44 +0100

    deployment Configurations

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ozz%wo!x#c=x3vs*rn%a3c0l51hvpf1k9f2y53unt6!be@(y0^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
